8007f3a02cc71d1776a4ec20788e2ab33bf7d918
LeeYeJin <dldpwls0626@gmail.com>
2024-09-02 14:31:20 +0900

    chat + llm chatting history

--- MY/inflearn_llm/streamlit_chatbot/chat.py
+++ MY/inflearn_llm/streamlit_chatbot/chat.py
@@ -1,7 +1,8 @@
 import streamlit as st 
 import os
 from dotenv import load_dotenv
-from llm import get_ai_message
+from llm import get_ai_response
+
 st.set_page_config(page_title="소득세 챗봇", page_icon="🤖")
 st.title("🤖 소득세 챗봇")
 st.caption("소득세에 관련된 모든것을 답해드립니다!")
@@ -25,8 +26,8 @@ if user_question := st.chat_input(placeholder="소득세에 관련된 궁금한 
     st.session_state.message_list.append({"role" : "user", "content": user_question})
     
     with st.spinner("답변을 생성하는 중입니다~"):
-        ai_message = get_ai_message(user_question)
+        ai_response = get_ai_response(user_question)
         with st.chat_message("ai"):
-            st.write(ai_message)
+            ai_message = st.write_stream(ai_response)
         st.session_state.message_list.append({"role" : "ai", "content": ai_message})
 # print(f"after == {st.session_state.message_list}")        
